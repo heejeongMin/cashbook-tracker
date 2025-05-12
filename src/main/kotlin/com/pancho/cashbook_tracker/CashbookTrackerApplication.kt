efde334502c9bb9c345d8a1010e4d66bdf683869
heejeongMin <hj.min1031@gmail.com>
2025-05-12 17:29:40 +0900

    init

---
+++ src/main/kotlin/com/pancho/cashbook_tracker/CashbookTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.pancho.cashbook_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CashbookTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CashbookTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/pancho/cashbook_tracker/CashbookTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pancho.cashbook_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CashbookTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
